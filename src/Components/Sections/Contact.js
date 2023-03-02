a4727b3a9a02c138354c7a78801b32cb8310d737
Giuliano Maradei <giulianomaradei@gmail.com>
2023-03-02 17:36:16 -0300

    s

--- src/Components/Sections/Contact.js
+++ src/Components/Sections/Contact.js
@@ -1,14 +1,26 @@
 import Section from '../UI/Section';
 import './Contact.css';
 
+import copy_button from '../../Assets/UI/copy_button.png';
+/* import linkedin_logo from '../../Assets/Logos/linkedin_logo.png';
+import github_logo from '../../Assets/Logos/github_logo.png'; */
+
 function Contact(){
     return(
         <Section id="contact">
             <div className='contact-title'>Contato</div>
             <div className='contact-container'>
                 <div className='email-phone'>
-                    <div>Email: giulianomaradei@gmail.com</div>
-                    <div>Phone: +55 34 99869-7052</div>
+                    <div className='contact-line'>
+                        <div id='email'>Email: giulianomaradei@gmail.com</div>
+                        <button>
+                            <img src={copy_button} className="copy-button" alt="copy_button"></img>
+                        </button>
+                    </div>
+                    <div className='contact-line'>
+                        <div id='phone'>Phone: +55 34 99869-7052</div>
+                        <img src={copy_button} className="copy-button" alt="copy_button"></img>
+                    </div>
                 </div>
                 <div className='social-media'>teste</div>
             </div>
