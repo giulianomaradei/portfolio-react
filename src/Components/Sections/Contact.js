f4471da241bf2d403f5b11c5d5ef5a9bc9818d4c
Giuliano Maradei <72819676+giulianomaradei@users.noreply.github.com>
2023-03-02 19:50:48 -0300

    s

--- src/Components/Sections/Contact.js
+++ src/Components/Sections/Contact.js
@@ -1,27 +1,61 @@
+import React,{useState} from 'react';
 import Section from '../UI/Section';
 import './Contact.css';
 
 import copy_button from '../../Assets/UI/copy_button.png';
-/* import linkedin_logo from '../../Assets/Logos/linkedin_logo.png';
-import github_logo from '../../Assets/Logos/github_logo.png'; */
+import check_button from '../../Assets/UI/check_button.png';
+
+import linkedin_logo from '../../Assets/Logos/linkedin_logo.png';
+import github_logo from '../../Assets/Logos/github_logo.png';
 
 function Contact(){
+
+    const [emailButton,setEmailButton] = useState(copy_button);
+    const [phoneButton,setPhoneButton] = useState(copy_button);
+    
+
+    function copyToClipboard(event){
+        if(event.target.id === "email"){
+            navigator.clipboard.writeText("giulianomaradei@gmail.com");
+            setEmailButton(check_button);
+            setTimeout(()=>{
+                setEmailButton(copy_button)
+            },2000);
+        }else{
+            navigator.clipboard.writeText("+5534998697052");
+            setPhoneButton(check_button);
+            setTimeout(()=>{
+                setPhoneButton(copy_button)
+            },2000);
+        }
+        
+    }
+
+
     return(
         <Section id="contact">
             <div className='contact-title'>Contato</div>
             <div className='contact-container'>
                 <div className='email-phone'>
                     <div className='contact-line'>
-                        <div id='email'>Email: giulianomaradei@gmail.com</div>
-                        <img src={copy_button} className="copy-button" alt="copy_button"></img>
+                        <div><strong>E-mail:</strong> giulianomaradei@gmail.com</div>
+                        <button onClick={copyToClipboard}>
+                            <img id='email' src={emailButton} className="copy-button" alt="copy_button"></img>
+                        </button>
                         
                     </div>
                     <div className='contact-line'>
-                        <div id='phone'>Phone: +55 34 99869-7052</div>
-                        <img src={copy_button} className="copy-button" alt="copy_button"></img>
+                        <div ><strong>Fone:</strong> +55 34 99869-7052</div>
+                        <button onClick={copyToClipboard}>
+                            <img id='phone' src={phoneButton} className="copy-button" alt="copy_button"></img>
+                        </button>
                     </div>
                 </div>
-                <div className='social-media'>teste</div>
+
+                <div className='social-media'>
+                    <img src={linkedin_logo} className="social-media" alt="linkedin_logo"></img>
+                    <img src={github_logo} className="social-media" alt="github_logo"></img>
+                </div>
             </div>
         </Section>
     )
