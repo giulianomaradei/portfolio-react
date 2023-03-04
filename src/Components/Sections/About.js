7d73c7b12c53e2d3d90679ce633d36eb0ce431b8
Giuliano Maradei <giulianomaradei@gmail.com>
2023-03-04 07:57:32 -0300

    s

--- src/Components/Sections/About.js
+++ src/Components/Sections/About.js
@@ -8,7 +8,7 @@ function About(){
             <div className='about-title'>Sobre mim</div>
             <div className='about-container'>
                 <img className='image' src={perfil} alt="eu"></img>
-                <div className='info'>Meu nome é Giuliano Maradei, sou estudante do curso de Sistemas da Informação na UFU e de desenvolvimento Full-Stack. Amo cirar sites e aplicações, lidar com problemas logicos e algoritmos complexos</div>  
+                <div className='info'>Meu nome é Giuliano Maradei, graduando em Sistemas da Informação na UFU e estudante de desenvolvimento Full-Stack. Amo criar sites e aplicações, lidar com problemas logicos e algoritmos complexos</div>  
             </div>
         </Section>
     )
